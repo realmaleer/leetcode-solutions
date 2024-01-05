a3d201bebc64363dab831fff86f84d3250b223d3
realmaleer <78098369+realmaleer@users.noreply.github.com>
2024-01-05 00:20:45 -0500

    leetcode #455

--- C#/455_AssignCookies.cs
+++ C#/455_AssignCookies.cs
@@ -1,35 +1,35 @@
 public class Solution
+{
+    public int FindContentChildren(int[] g, int[] s)
     {
-        public int FindContentChildren(int[] g, int[] s)
+        Array.Sort(g, (a, b) => b - a);
+        Array.Sort(s, (a, b) => b - a);
+
+        int nextChildren = 0;
+        int numberOfContentChildren = 0;
+
+        for (int i = 0; i < s.Length; i++)
         {
-            Array.Sort(g, (a, b) => b - a);
-            Array.Sort(s, (a, b) => b - a);
-
-            int nextChildren = 0;
-            int numberOfContentChildren = 0;
-
-            for (int i = 0; i < s.Length; i++)
+            for (int j = nextChildren; j < g.Length; j++)
             {
-                for (int j = nextChildren; j < g.Length; j++)
+                if (s[i] >= g[j])
                 {
-                    if (s[i] >= g[j])
-                    {
-                        numberOfContentChildren++;
-                        if (j == g.Length - 1)
-                        {
-                            return numberOfContentChildren;
-                        }
-                        nextChildren = j + 1;
-                        break;
-                    }
-
+                    numberOfContentChildren++;
                     if (j == g.Length - 1)
                     {
                         return numberOfContentChildren;
                     }
+                    nextChildren = j + 1;
+                    break;
+                }
+
+                if (j == g.Length - 1)
+                {
+                    return numberOfContentChildren;
                 }
             }
-
-            return numberOfContentChildren;
         }
+
+        return numberOfContentChildren;
     }
+}
